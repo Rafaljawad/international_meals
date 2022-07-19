fae009fb772ee7550a0d4ee9a0eb5c3656a3b825
Rafal Jawad <rafal.qasim@gmail.com>
2022-07-19 01:21:53 -0400

    added alert box to delete or nor delete meal , and make shadow for the cards on all meal page using js

--- flask_app/static/js/scripts.js
+++ flask_app/static/js/scripts.js
@@ -1,14 +1,23 @@
-function remove_sigin_btn(){
-    var x= document.querySelectorAll(".nav-item ")[2]
-    console.log(x);
-    x.classList.add("hidden");
-}
+// function remove_sigin_btn(){
+//     var x= document.querySelectorAll(".nav-item ")[2]
+//     console.log(x);
+//     x.classList.add("hidden");
+// }
 
 
 function alertBox(){
     document.querySelector(".alert").classList.remove("hidden")
+    document.querySelector("main").classList.add("overlay")
 }
 function deleteMeal(){
     document.querySelector(".alert").classList.add("hidden")
+    document.querySelector("main").classList.remove("overlay")
 
+}
+
+function makeShadow(element){
+    element.classList.add("shadow")
+} 
+function removeShadow(element){
+    element.classList.remove("shadow")
 }
